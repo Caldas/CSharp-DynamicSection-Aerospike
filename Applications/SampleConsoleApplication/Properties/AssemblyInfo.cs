979605de6099d84af6437956affc03849f87b218
Caldas <fabio.caldas@vtex.com.br>
2015-08-04 21:55:41 -0300

    Created sample application program #7

---
+++ Applications/SampleConsoleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sample Console Application")]
+[assembly: AssemblyDescription("Sample console application for test Aerospike library to DynamicConfigurationSection project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VTEX")]
+[assembly: AssemblyProduct("DynamicConfigurationSection")]
+[assembly: AssemblyCopyright("Copyright © VTEX 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("b73bf014-0ee9-4e8d-81ed-af2476fbb51e")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
